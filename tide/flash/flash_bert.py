f8e823d69791b5b1d6ef289e45cd24652eb6aecd
Ferdinand Schlatt <fschlatt@gmail.com>
2024-02-08 13:18:17 +0100

    add flash_attn

--- tide/flash/flash_bert.py
+++ tide/flash/flash_bert.py
@@ -1,7 +1,12 @@
-import torch
 from typing import Tuple
 
+import torch
 from transformers.models.bert.modeling_bert import BertSelfAttention
+
+try:
+    from flash_attn import flash_attn_func
+except ImportError:
+    flash_attn_func = None
 
 from .flash_mixin import FlashMixin
 
@@ -23,13 +28,22 @@ class FlashBertMixin(FlashMixin):
         key = self.transpose_for_scores(self.key(key_value_hidden_states))
         value = self.transpose_for_scores(self.value(key_value_hidden_states))
 
-        context = torch.nn.functional.scaled_dot_product_attention(
-            query,
-            key,
-            value,
-            attention_mask.to(query.dtype) if attention_mask is not None else None,
-            self.dropout.p if self.training else 0,
-        )
+        if (
+            flash_attn_func is not None
+            and hidden_states.is_cuda
+            and (attention_mask is None or not attention_mask.any())
+        ):
+            context = flash_attn_func(
+                query, key, value, self.dropout.p if self.training else 0
+            )
+        else:
+            context = torch.nn.functional.scaled_dot_product_attention(
+                query,
+                key,
+                value,
+                attention_mask.to(query.dtype) if attention_mask is not None else None,
+                self.dropout.p if self.training else 0,
+            )
 
         context = context.permute(0, 2, 1, 3).contiguous()
         new_context_shape = context.size()[:-2] + (self.all_head_size,)
