773bf76ff9a3924eeb52fb044fda093d40cb1a7d
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2024-04-02 09:58:46 +0200

    move to gpu only if gpu is available

--- mvr/indexer.py
+++ mvr/indexer.py
@@ -86,7 +86,8 @@ class Indexer(ABC):
             self.mvr_config.embedding_dim, index_factory, self.metric_type
         )
 
-        self.to_gpu()
+        if torch.cuda.is_available():
+            self.to_gpu()
 
         self.set_verbosity()
 
