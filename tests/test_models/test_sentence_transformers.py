91cee642986b0a0adfa80362361ac21ad64b6d8a
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2024-07-09 15:59:56 +0200

    test scoring for sentence transformer

--- tests/test_models/test_sentence_transformers.py
+++ tests/test_models/test_sentence_transformers.py
@@ -1,26 +1,39 @@
 import torch
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
 
 from lightning_ir import BiEncoderModel
 
 
 def test_same_as_sentence_transformer():
-    sentences = ["This is an example sentence", "Each sentence is converted"]
+    query = "This is an example query"
+    docs = ["This is an example sentence", "Each sentence is converted"]
 
-    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    model_name = "sentence-transformers/msmarco-MiniLM-L-6-v3"
     model = BiEncoderModel.from_pretrained(
         model_name, add_marker_tokens=False, projection=None, embedding_dim=384
     )
     tokenizer = BiEncoderModel.config_class.tokenizer_class.from_pretrained(
         model_name, **model.config.to_tokenizer_dict()
     )
-    encoding = tokenizer.tokenize_doc(sentences, return_tensors="pt", padding=True)
+    encodings = tokenizer.tokenize(query, docs, return_tensors="pt", padding=True)
     with torch.no_grad():
-        embeddings = model.encode_doc(**encoding)
+        output = model.forward(encodings["query_encoding"], encodings["doc_encoding"])
 
     orig_model = SentenceTransformer(model_name)
-    orig_embeddings = orig_model.encode(sentences)
+    orig_query_embeddings = orig_model.encode(query)
+    orig_doc_embeddings = orig_model.encode(docs)
+    orig_scores = util.dot_score(
+        torch.from_numpy(orig_query_embeddings), torch.from_numpy(orig_doc_embeddings)
+    )
 
     assert torch.allclose(
-        embeddings.embeddings.squeeze(1), torch.from_numpy(orig_embeddings), atol=1e-6
+        output.query_embeddings.embeddings.squeeze(1),
+        torch.from_numpy(orig_query_embeddings),
+        atol=1e-6,
     )
+    assert torch.allclose(
+        output.doc_embeddings.embeddings.squeeze(1),
+        torch.from_numpy(orig_doc_embeddings),
+        atol=1e-6,
+    )
+    assert torch.allclose(output.scores, orig_scores.squeeze(0), atol=1e-6)
