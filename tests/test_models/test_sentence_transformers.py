398821392d9a7e668e6cc6dc111dac0e5b3a3a68
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2024-07-12 10:19:16 +0200

    add bert test

--- tests/test_models/test_sentence_transformers.py
+++ tests/test_models/test_sentence_transformers.py
@@ -1,23 +1,22 @@
+import pytest
 import torch
 from sentence_transformers import SentenceTransformer, util
 
 from lightning_ir import BiEncoderModel
 
 
-def test_same_as_sentence_transformer():
+@pytest.mark.parametrize(
+    "model_name",
+    [
+        "sentence-transformers/msmarco-bert-base-dot-v5",
+        "sentence-transformers/msmarco-MiniLM-L-6-v3",
+    ],
+    ids=["bert", "minilm"],
+)
+def test_same_as_sentence_transformer(model_name: str):
+    # TODO why does this test fail?!
     query = "This is an example query"
     docs = ["This is an example sentence", "Each sentence is converted"]
-
-    model_name = "sentence-transformers/msmarco-MiniLM-L-6-v3"
-    model = BiEncoderModel.from_pretrained(
-        model_name, add_marker_tokens=False, projection=None, embedding_dim=384
-    )
-    tokenizer = BiEncoderModel.config_class.tokenizer_class.from_pretrained(
-        model_name, **model.config.to_tokenizer_dict()
-    )
-    encodings = tokenizer.tokenize(query, docs, return_tensors="pt", padding=True)
-    with torch.no_grad():
-        output = model.forward(encodings["query_encoding"], encodings["doc_encoding"])
 
     orig_model = SentenceTransformer(model_name)
     orig_query_embeddings = orig_model.encode(query)
@@ -25,6 +24,19 @@ def test_same_as_sentence_transformer():
     orig_scores = util.dot_score(
         torch.from_numpy(orig_query_embeddings), torch.from_numpy(orig_doc_embeddings)
     )
+
+    model = BiEncoderModel.from_pretrained(
+        model_name,
+        add_marker_tokens=False,
+        projection=None,
+        embedding_dim=orig_query_embeddings.shape[-1],
+    )
+    tokenizer = BiEncoderModel.config_class.tokenizer_class.from_pretrained(
+        model_name, **model.config.to_tokenizer_dict()
+    )
+    encodings = tokenizer.tokenize(query, docs, return_tensors="pt", padding=True)
+    with torch.no_grad():
+        output = model.forward(encodings["query_encoding"], encodings["doc_encoding"])
 
     assert torch.allclose(
         output.query_embeddings.embeddings.squeeze(1),
