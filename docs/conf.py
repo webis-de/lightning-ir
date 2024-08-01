13db96cb49edaa45f21b441f969819280bea9575
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2024-08-01 17:30:19 +0200

    sort imports

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,9 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-from typing import Any, Dict
-import sys
 import os
+import sys
+from typing import Any, Dict
 
 from sphinxawesome_theme.postprocess import Icons
 
