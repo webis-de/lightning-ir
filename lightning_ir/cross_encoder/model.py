f5fc3ffc15155c6709c5d1b682baa267f6cadd77
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2024-09-04 16:04:24 +0200

    fix cross encoder sub query batching

--- lightning_ir/cross_encoder/model.py
+++ lightning_ir/cross_encoder/model.py
@@ -22,37 +22,41 @@ class CrossEncoderModel(LightningIRModel):
         super().__init__(config, *args, **kwargs)
         self.config: CrossEncoderConfig
         self.linear = torch.nn.Linear(config.hidden_size, 1, bias=config.linear_bias)
+        self._sub_batch_size: int | None = None
 
     def batched_backbone_forward(self, encoding: BatchEncoding) -> torch.Tensor:
         if not self.ALLOW_BATCHING:
-            return self.backbone_forward(encoding)
-        batch_size = encoding["input_ids"].shape[0]
+            return self.backbone_forward(**encoding)
+        if self._sub_batch_size is None:
+            self._sub_batch_size = encoding.input_ids.shape[0]
         outputs = []
         sub_encoding = encoding
+        remaining_encoding = encoding
         while True:
             try:
-                outputs.append(self.backbone_forward(sub_encoding).last_hidden_state)
+                num_batches = -(remaining_encoding.input_ids.shape[0] // -self._sub_batch_size)
+                for _ in range(num_batches):
+                    sub_encoding = BatchEncoding(
+                        {key: value[: self._sub_batch_size] for key, value in remaining_encoding.items()}
+                    )
+                    outputs.append(self.backbone_forward(**sub_encoding).last_hidden_state)
+                    remaining_encoding = BatchEncoding(
+                        {key: value[self._sub_batch_size :] for key, value in remaining_encoding.items()}
+                    )
                 break
             except RuntimeError as e:
-                if "CUDA out of memory" in str(e):
-                    batch_size = batch_size // 2
-                    if batch_size == 0:
+                if "CUDA out of memory" in str(e) or "CUDACachingAllocator.cpp" in str(e):
+                    self._sub_batch_size = self._sub_batch_size // 2
+                    if self._sub_batch_size == 0:
                         raise e
-                    sub_encoding = BatchEncoding(**{key: value[:batch_size] for key, value in sub_encoding.items()})
                 else:
                     raise e
-        if batch_size == encoding["input_ids"].shape[0]:
-            return outputs[0]
-        num_batches = encoding["input_ids"].shape[0] // batch_size - 1
-        for i in range(1, num_batches):
-            sub_encoding = BatchEncoding(
-                **{key: value[batch_size * i : batch_size * (i + 1)] for key, value in encoding.items()}
-            )
-            outputs.append(self.backbone_forward(sub_encoding).last_hidden_state)
-        return torch.cat(outputs)
+        output = torch.cat(outputs)
+        assert output.shape[0] == encoding.input_ids.shape[0]
+        return output
 
     def forward(self, encoding: BatchEncoding) -> CrossEncoderOutput:
-        embeddings = self.batched_backbone_forward(**encoding)
+        embeddings = self.batched_backbone_forward(encoding)
         embeddings = self._pooling(
             embeddings, encoding.get("attention_mask", None), pooling_strategy=self.config.pooling_strategy
         )
