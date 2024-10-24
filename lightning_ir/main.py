8039a4c9e1fdb11b24cc4b4f06152d218090b0e5
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2024-10-24 09:30:25 +0200

    remove data argument linking

--- lightning_ir/main.py
+++ lightning_ir/main.py
@@ -96,7 +96,6 @@ class LightningIRCLI(LightningCLI):
         parser.add_lr_scheduler_args(tuple(LR_SCHEDULERS))
         parser.link_arguments("model.init_args.model_name_or_path", "data.init_args.model_name_or_path")
         parser.link_arguments("model.init_args.config", "data.init_args.config")
-        parser.link_arguments("model", "data.init_args.model", apply_on="instantiate")
         parser.link_arguments("trainer.max_steps", "lr_scheduler.init_args.num_training_steps")
 
     @staticmethod
