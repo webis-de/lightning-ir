b1895a72ba31c903b76f61669c14b04aa56c0800
lisa lehner <lilehner@t-online.de>
2024-11-28 16:19:29 +0100

    #3 config und module

--- lightning_ir/models/mvr/config.py
+++ lightning_ir/models/mvr/config.py
@@ -5,5 +5,10 @@ class MVRConfig(BiEncoderConfig):
 
     ADDED_ARGS = BiEncoderConfig.ADDED_ARGS.union({"additional_linear_layer"})
 
-    def __init__(self, **kwargs):
+    def __init__(self, 
+                 add_viewer_tokens = True, 
+                 num_viewer_tokens = 8,
+                 **kwargs):
         super().__init__(**kwargs)
+        self.add_viewer_tokens = add_viewer_tokens   
+        self.num_viewer_tokens = num_viewer_tokens     
--- lightning_ir/models/mvr/module.py
+++ lightning_ir/models/mvr/module.py
@@ -1,6 +1,8 @@
 class MVRModule(BiEncoderModule):
     def __init__(
             self,
-
+            
     ):
         super().__init__()
+        if self.config.add_viewer_tokens and len(self.tokenizer) > self.config.vocab_size:
+            self.model.resize_token_embeddings(len(self.tokenizer), 8)
