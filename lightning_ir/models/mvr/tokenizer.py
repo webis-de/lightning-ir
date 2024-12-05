b9111654e32a7bda33db08c15dd911b8e3026b76
lisa lehner <lilehner@t-online.de>
2024-12-05 16:14:08 +0100

    #6 tokens als liste

--- lightning_ir/models/mvr/tokenizer.py
+++ lightning_ir/models/mvr/tokenizer.py
@@ -7,11 +7,11 @@ class MVRTokenizer(BiEncoderTokenizer):
     ):
         super().__init__()
         if num_viewer_tokens is not None:
-            viewer_tokens = " ".join(f"[VIE{idx}]" for idx in range(num_viewer_tokens))
+            viewer_tokens = [f"[VIE{idx}]" for idx in range(num_viewer_tokens)]
             self.add_tokens(viewer_tokens, special_tokens=True)
-            viewer_token_ids = [(f"[VIE{viewer_token_id}]", self.viewer_token_id(f"[VIE{viewer_token_id}]")) for viewer_token_id in range(num_viewer_tokens)]
+            viewer_token_ids = [(viewer_tokens[viewer_token_id], self.viewer_token_id(viewer_token_id)) for viewer_token_id in range(num_viewer_tokens)]
             self.doc_post_processor = TemplateProcessing(
-                single=f"{viewer_tokens} $0 [SEP]",
+                single=f"{" ".join(viewer_tokens)} $0 [SEP]",
                 pair=f"[CLS] {self.QUERY_TOKEN} $A [SEP] $B:1 [SEP]:1",
                 special_tokens=[
                     ("[CLS]", self.cls_token_id),
