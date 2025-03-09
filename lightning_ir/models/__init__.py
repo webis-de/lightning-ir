90a0425e9f373c374f47027174e61bfc8ede78e3
samiki-hub <samwel.mueller2003@gmx.de>
2025-03-09 10:54:03 +0100

    add coil

--- lightning_ir/models/__init__.py
+++ lightning_ir/models/__init__.py
@@ -1,4 +1,5 @@
 from .col import ColConfig, ColModel
+from .coil import CoilConfig, CoilEmbedding, CoilModel, CoilScoringFunction
 from .splade import SpladeConfig, SpladeModel
 from .t5 import T5CrossEncoderConfig, T5CrossEncoderModel, T5CrossEncoderTokenizer
 from .xtr import XTRConfig, XTRModel
@@ -6,6 +7,10 @@ from .xtr import XTRConfig, XTRModel
 __all__ = [
     "ColConfig",
     "ColModel",
+    "CoilConfig",
+    "CoilEmbedding",
+    "CoilModel",
+    "CoilScoringFunction"
     "SpladeConfig",
     "SpladeModel",
     "T5CrossEncoderConfig",
