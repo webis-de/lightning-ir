aeacc2c63b6470705649291e8d6e2aa4d1db9729
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2025-03-26 14:49:27 +0100

    use mean for flops

--- lightning_ir/loss/loss.py
+++ lightning_ir/loss/loss.py
@@ -477,8 +477,8 @@ class L1Regularization(RegularizationLossFunction):
 class FLOPSRegularization(RegularizationLossFunction):
     def compute_loss(self, output: BiEncoderOutput) -> torch.Tensor:
         query_embeddings, doc_embeddings = self.process_embeddings(output)
-        query_loss = torch.sum(torch.mean(torch.abs(query_embeddings), dim=0) ** 2)
-        doc_loss = torch.sum(torch.mean(torch.abs(doc_embeddings), dim=0) ** 2)
-        anti_zero = 1 / (torch.sum(query_embeddings) ** 2) + 1 / (torch.sum(doc_embeddings) ** 2)
+        query_loss = torch.mean(torch.mean(torch.abs(query_embeddings), dim=0) ** 2)
+        doc_loss = torch.mean(torch.mean(torch.abs(doc_embeddings), dim=0) ** 2)
+        anti_zero = 1 / (torch.mean(query_embeddings) ** 2) + 1 / (torch.mean(doc_embeddings) ** 2)
         loss = self.query_weight * query_loss + self.doc_weight * doc_loss + anti_zero
         return loss
