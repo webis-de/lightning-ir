8730bfda7d18b7dfb41a9a82b05df5f97e2f529a
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2025-06-11 15:49:03 +0200

    flake8 + black + add pylate dependency

--- tests/test_models/test_col.py
+++ tests/test_models/test_col.py
@@ -5,7 +5,6 @@ transformers.AdamW = None
 
 from colbert.modeling.checkpoint import Checkpoint  # noqa: E402
 from colbert.modeling.colbert import ColBERTConfig, colbert_score  # noqa: E402
-
 from pylate import models, rank
 
 from lightning_ir import BiEncoderModule  # noqa: E402
@@ -55,16 +54,16 @@ def test_same_as_modern_colbert():
     doc_embedding = output.doc_embeddings
 
     orig_model = models.ColBERT(model_name_or_path=model_name)
-    orig_query = orig_model.encode(
-        [query],
-        is_query=True,
+    orig_query = orig_model.encode([query], is_query=True)
+    orig_docs = orig_model.encode([documents], is_query=False)
+    orig_scores = rank.rerank(
+        queries_embeddings=orig_query, documents_embeddings=orig_docs, documents_ids=[list(range(len(documents)))]
     )
-    orig_docs = orig_model.encode(
-        [documents],
-        is_query=False,
-    )
-    orig_scores = rank.rerank(queries_embeddings=orig_query, documents_embeddings=orig_docs, documents_ids=[list(range(len(documents)))])
 
     assert torch.allclose(query_embedding.embeddings, torch.tensor(orig_query[0]), atol=1e-6)
-    assert torch.allclose(doc_embedding.embeddings[doc_embedding.scoring_mask], torch.cat([torch.from_numpy(d) for doc in orig_docs for d in doc]), atol=1e-6)
+    assert torch.allclose(
+        doc_embedding.embeddings[doc_embedding.scoring_mask],
+        torch.cat([torch.from_numpy(d) for doc in orig_docs for d in doc]),
+        atol=1e-6,
+    )
     assert torch.allclose(output.scores, torch.tensor([d["score"] for q in orig_scores for d in q]), atol=1e-6)
