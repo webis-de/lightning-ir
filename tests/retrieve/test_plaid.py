aec970b012d7dcb17f0c0fc42ed8d7570a891433
Rayk Kretzschmar <112646288+RaykKretzschmar@users.noreply.github.com>
2025-08-27 11:49:38 +0200

    Add PLAID Fixes #5 Enhance test_plaid to include document retrieval and comparison between pylate and lightning_ir

--- tests/retrieve/test_plaid.py
+++ tests/retrieve/test_plaid.py
@@ -1,11 +1,97 @@
-from pylate import indexes, models
+from pylate import indexes, models, retrieve
 from lightning_ir.retrieve.plaid.plaid_indexer import PlaidIndexer, PlaidIndexConfig
-from lightning_ir import IndexBatch, BiEncoderModule
+from lightning_ir.retrieve.plaid.plaid_searcher import PlaidSearcher, PlaidSearchConfig
+from lightning_ir import IndexBatch, SearchBatch, BiEncoderModule
 from pathlib import Path
 import numpy as np
 import torch
 
 from tests.conftest import CORPUS_DIR, DATA_DIR
+
+
+def test_plaid():
+    model_name = "lightonai/GTE-ModernColBERT-v1"
+    query = "What is the capital of France?"
+
+    pylate_model = models.ColBERT(
+        model_name_or_path=model_name,
+    )
+
+    pylate_index = indexes.PLAID(
+        index_folder=Path(DATA_DIR) / "indexes/pylate-index",
+        index_name="index",
+        override=True,
+    )
+
+    documents_path = Path(CORPUS_DIR) / "docs.tsv"
+    with documents_path.open("r", encoding="utf-8") as f:
+        documents_ids, documents = zip(*[doc.strip().split("\t") for doc in f.readlines()])
+
+    pylate_documents_embeddings = pylate_model.encode(
+        documents,
+        batch_size=32,
+        is_query=False,
+        show_progress_bar=False,
+    )
+
+    pylate_index.add_documents(
+        documents_ids=documents_ids,
+        documents_embeddings=pylate_documents_embeddings,
+    )
+
+    retriever = retrieve.ColBERT(index=pylate_index)
+
+    pylate_queries_embeddings = pylate_model.encode(
+        [query],
+        batch_size=32,
+        is_query=True,
+        show_progress_bar=True,
+    )
+
+    pylate_scores = retriever.retrieve(
+        queries_embeddings=pylate_queries_embeddings,
+        k=10,  # Retrieve the top 10 matches for each query
+    )
+
+    module = BiEncoderModule(model_name).eval()
+    index_batch = IndexBatch(doc_ids=documents_ids, docs=documents)
+    with torch.inference_mode():
+        output = module(index_batch)
+
+    index_dir = Path(DATA_DIR) / "indexes/lightning-ir-index"
+    index_config = PlaidIndexConfig(num_centroids=512)
+    lightning_index = PlaidIndexer(
+        index_dir=index_dir,
+        index_config=index_config,
+        module=module,
+        verbose=True,
+    )
+
+    import faiss
+
+    faiss.omp_set_num_threads(1)
+
+    lightning_index.add(index_batch=index_batch, output=output)
+    lightning_index.save()
+
+    search_batch = SearchBatch(query_ids=["1"], queries=[query], doc_ids=documents_ids)
+    search_config = PlaidSearchConfig(k=10)
+    lightning_searcher = PlaidSearcher(
+        index_dir=DATA_DIR / "indexes/lightning-ir-index", search_config=search_config, module=module
+    )
+
+    lightning_query_embedding = module(search_batch)
+    lightning_scores = lightning_searcher.search(lightning_query_embedding)
+
+    pylate_top_scores = np.array([doc["score"] for doc in pylate_scores[0][:5]])
+    lightning_top_scores = lightning_scores[0][:5].detach().cpu().numpy()
+
+    pylate_top_ids = [doc["id"] for doc in pylate_scores[0][:5]]
+    lightning_top_ids = [documents_ids[i] for i in np.argsort(-lightning_scores[0].detach().cpu().numpy())[:5]]
+    assert pylate_top_ids == lightning_top_ids, "Top document IDs do not match between pylate and lightning_ir"
+    assert np.allclose(
+        pylate_top_scores, lightning_top_scores, atol=0.5
+    ), "Top scores do not match closely between pylate and lightning_ir"
 
 
 def test_plaid_indexing():
