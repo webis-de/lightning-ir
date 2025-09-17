5b1f0a360aa4b8cab01d7acbf0a54a199c01e7e0
Rayk Kretzschmar <112646288+RaykKretzschmar@users.noreply.github.com>
2025-09-17 21:59:10 +0200

    Add PLAID Fixes #5 Add Plaid indexer and searcher implementations using fast-plaid library

---
+++ lightning_ir/retrieve/plaid/plaid_indexer_fastplaid.py
@@ -0,0 +1,73 @@
+"""Plaid Indexer using fast-plaid library for Lightning IR Framework"""
+
+from pathlib import Path
+from fast_plaid import search
+
+from ...bi_encoder import BiEncoderModule, BiEncoderOutput
+from ...data import IndexBatch
+from ...models import ColConfig
+from ..base import IndexConfig, Indexer
+
+
+class PlaidIndexerFastPlaid(Indexer):
+    """Indexer for Plaid using fast-plaid library."""
+
+    def __init__(
+        self,
+        index_dir: Path,
+        index_config: "PlaidIndexConfig",
+        module: BiEncoderModule,
+        verbose: bool = False,
+    ) -> None:
+        super().__init__(index_dir, index_config, module, verbose)
+        self.index_config: PlaidIndexConfig
+        self.index = None
+
+    def add(self, index_batch: IndexBatch, output: BiEncoderOutput) -> None:
+        doc_embeddings = output.doc_embeddings.embeddings.detach()
+        if doc_embeddings is None:
+            raise ValueError("Expected doc_embeddings in BiEncoderOutput")
+
+        if not self.index:
+            self.index = search.FastPlaid(index=str(self.index_dir))
+            self.index.create(
+                documents_embeddings=doc_embeddings,
+                kmeans_niters=self.index_config.k_means_iters,
+                nbits=self.index_config.n_bits,
+                n_samples_kmeans=self.index_config.num_train_embeddings,
+            )
+        else:
+            self.index.update(documents_embeddings=doc_embeddings)
+
+
+class PlaidIndexConfig(IndexConfig):
+    """Configuration class for Plaid indexers in the Lightning IR framework."""
+
+    indexer_class = PlaidIndexerFastPlaid
+    SUPPORTED_MODELS = {ColConfig.model_type}
+
+    def __init__(
+        self,
+        num_centroids: int,
+        num_train_embeddings: int | None = None,
+        k_means_iters: int = 4,
+        n_bits: int = 2,
+        seed: int = 42,
+    ) -> None:
+        """Initialize the PlaidIndexConfig.
+
+        Args:
+            num_centroids (int): Number of centroids for the Plaid index.
+            num_train_embeddings (int | None): Number of embeddings to use for training the index. If None, it will
+                be set later. Defaults to None.
+            k_means_iters (int): Number of iterations for k-means clustering. Defaults to 4.
+            n_bits (int): Number of bits for the residual codec. Defaults to 2.
+            seed (int): Random seed for reproducibility. Defaults to 42.
+        """
+        super().__init__()
+        max_points_per_centroid = 256
+        self.num_centroids = num_centroids
+        self.num_train_embeddings = num_train_embeddings or num_centroids * max_points_per_centroid
+        self.k_means_iters = k_means_iters
+        self.n_bits = n_bits
+        self.seed = seed
---
+++ lightning_ir/retrieve/plaid/plaid_searcher_fastplaid.py
@@ -0,0 +1,80 @@
+"""Plaid Searcher using fast-plaid library for Lightning IR Framework"""
+
+from pathlib import Path
+
+from fast_plaid import search
+import torch
+
+from ...bi_encoder import BiEncoderModule, BiEncoderOutput
+from ...models import ColConfig
+from ..base import SearchConfig, Searcher
+
+
+class PlaidSearcherFastPlaid(Searcher):
+    """Searcher for Plaid using fast-plaid library."""
+
+    def __init__(
+        self,
+        index_dir: Path,
+        search_config: "PlaidSearchConfig",
+        module: BiEncoderModule,
+        use_gpu: bool = False,
+    ) -> None:
+        # super().__init__(index_dir, search_config, module, use_gpu)
+        self.index_dir = index_dir
+        self.search_config = search_config
+        self.search_config: PlaidSearchConfig
+        self.index = None
+        self.use_gpu = use_gpu
+        self.module = module
+        self.device = torch.device("cuda") if use_gpu and torch.cuda.is_available() else torch.device("cpu")
+
+        # with open(self.index_dir / "doclens.0.json", "r") as doc_lens_f:
+        #     self.doc_lengths = json.load(doc_lens_f)
+
+        # super().to_gpu()
+
+    def load(self) -> None:
+        self.index = search.FastPlaid(index=str(self.index_dir))
+
+    def search(self, output: BiEncoderOutput):
+        if output.query_embeddings is None:
+            raise ValueError("Expected query_embeddings in BiEncoderOutput")
+
+        if not self.index:
+            raise ValueError("Index not loaded. Call load() before searching.")
+
+        scores = self.index.search(
+            queries_embeddings=output.query_embeddings.embeddings.detach(),
+            top_k=self.search_config.k,
+        )
+
+        return scores
+
+
+class PlaidSearchConfig(SearchConfig):
+    """Configuration class for Plaid searchers in the Lightning IR framework."""
+
+    search_class = PlaidSearcherFastPlaid
+    SUPPORTED_MODELS = {ColConfig.model_type}
+
+    def __init__(
+        self,
+        k: int,
+        candidate_k: int = 256,
+        n_cells: int = 1,
+        centroid_score_threshold: float = 0.5,
+    ) -> None:
+        """Initialize the PlaidSearchConfig.
+
+        Args:
+            k (int): Number of top documents to retrieve.
+            candidate_k (int): Number of candidate documents to consider for scoring. Defaults to 256.
+            n_cells (int): Number of cells to use for centroid retrieval. Defaults to 1.
+            centroid_score_threshold (float): Threshold for filtering candidates based on centroid scores.
+                Defaults to 0.5.
+        """
+        super().__init__(k)
+        self.candidate_k = candidate_k
+        self.n_cells = n_cells
+        self.centroid_score_threshold = centroid_score_threshold
