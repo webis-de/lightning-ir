cc03d47e375bfb88481785f0bf722d1c2e140789
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2025-10-06 13:23:42 +0200

    move rank-distillm to hf (#104)

--- lightning_ir/data/external_datasets/rank_distillm.py
+++ lightning_ir/data/external_datasets/rank_distillm.py
@@ -1,16 +1,14 @@
-from ir_datasets.util import GzipExtract
-
 from .ir_datasets_utils import ParquetScoredDocs, register_new_dataset
 
 
 def register_rank_distillm():
 
-    base_url = "https://zenodo.org/records/15753974/files/"
+    base_url = "https://huggingface.co/datasets/webis/rank-distillm/resolve/main/"
 
     dlc_contents = {
-        "url": f"{base_url}__rankzephyr-colbert-10000-" "sampled-100__msmarco-passage-train-judged.run?download=1",
-        "expected_md5": "49f8dbf2c1ee7a2ca1fe517eda528af6",
-        "cache_path": "msmarco-passage/train/rank-distillm-rankzephyr.run",
+        "url": f"{base_url}__rankzephyr-colbert-10000-sampled-100__msmarco-passage-train-judged.parquet",
+        "expected_md5": "02a245c712b4ea1804d0cb504005c0e2",
+        "cache_path": "msmarco-passage/train/rank-distillm-rankzephyr.parquet",
     }
     register_new_dataset(
         "msmarco-passage/train/rank-distillm-rankzephyr",
@@ -18,13 +16,13 @@ def register_rank_distillm():
         queries="msmarco-passage/train",
         qrels="msmarco-passage/train",
         scoreddocs=dlc_contents,
+        ScoreddocsType=ParquetScoredDocs,
     )
 
     dlc_contents = {
-        "url": f"{base_url}__set-encoder-colbert__" "msmarco-passage-train-judged.run.gz?download=1",
-        "expected_md5": "1f069d0daa9842a54a858cc660149e1a",
-        "cache_path": "msmarco-passage/train/rank-distillm-set-encoder.run",
-        "extractors": [GzipExtract],
+        "url": f"{base_url}__set-encoder-colbert-all-100__msmarco-passage-train-judged.parquet",
+        "expected_md5": "a47206da7dc551e3ebd4e5b6866be78a",
+        "cache_path": "msmarco-passage/train/rank-distillm-set-encoder.parquet",
     }
     register_new_dataset(
         "msmarco-passage/train/rank-distillm-set-encoder",
@@ -32,13 +30,13 @@ def register_rank_distillm():
         queries="msmarco-passage/train",
         qrels="msmarco-passage/train",
         scoreddocs=dlc_contents,
+        ScoreddocsType=ParquetScoredDocs,
     )
 
     dlc_contents = {
-        "url": f"{base_url}__monoelectra-colbert__" "msmarco-passage-train-judged.run.gz?download=1",
-        "expected_md5": "5abc9a6c2cdf986c0aedcea853f0b34c",
-        "cache_path": "msmarco-passage/train/rank-distillm-monoelectra.run",
-        "extractors": [GzipExtract],
+        "url": f"{base_url}__monoelectra-colbert-all-100__msmarco-passage-train-judged.parquet",
+        "expected_md5": "6561f33476a6c8408737f38ea85c848f",
+        "cache_path": "msmarco-passage/train/rank-distillm-monoelectra.parquet",
     }
     register_new_dataset(
         "msmarco-passage/train/rank-distillm-monoelectra",
@@ -46,10 +44,11 @@ def register_rank_distillm():
         queries="msmarco-passage/train",
         qrels="msmarco-passage/train",
         scoreddocs=dlc_contents,
+        ScoreddocsType=ParquetScoredDocs,
     )
 
     dlc_contents = {
-        "url": f"{base_url}__colbert__msmarco-passage-train-judged.parquet?download=1",
+        "url": f"{base_url}__colbert__msmarco-passage-train-judged.parquet",
         "expected_md5": "1e927d52af085516bf5a3de2865809d5",
         "cache_path": "msmarco-passage/train/rank-distillm-colbert.parquet",
     }
