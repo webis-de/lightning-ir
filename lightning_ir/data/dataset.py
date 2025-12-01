58a9e6104c8ec96fba61fcb380cba6a5d869cb24
Maik Fröbe <maik.froebe@uni-jena.de>
2025-12-01 18:04:48 +0100

    fix ruff stuff

--- lightning_ir/data/dataset.py
+++ lightning_ir/data/dataset.py
@@ -90,8 +90,10 @@ class IRDataset:
         try:
             from lsr_benchmark import register_to_ir_datasets
         except ImportError:
-            msg = "I could not import the lsr_benchmark package. Please install the lsr-benchmark via " + \
-                  f"'pip3 install lsr-benchmark' to load ir_datasets from there. I got the dataset id '{self.dataset}'."
+            msg = (
+                "I could not import the lsr_benchmark package. Please install the lsr-benchmark via "
+                + f"'pip3 install lsr-benchmark' to load ir_datasets from there. I got the dataset id '{self.dataset}'."
+            )
             print(msg)
             raise ValueError(msg)
 
