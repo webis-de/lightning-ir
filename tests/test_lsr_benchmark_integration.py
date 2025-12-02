8dd8aab5f64e74f61ce412fcdb5f29fe4a87b6a6
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2025-12-02 12:20:09 +0100

    user iter an len of dataset

--- tests/test_lsr_benchmark_integration.py
+++ tests/test_lsr_benchmark_integration.py
@@ -4,9 +4,8 @@ from lightning_ir import DocDataset, QueryDataset
 def test_lsr_benchmark_load_docs():
     dataset = DocDataset("lsr-benchmark/msmarco-passage/trec-dl-2019/judged")
     assert dataset is not None
-    all_docs = [i for i in dataset]
-    assert "8811478" == all_docs[0].doc_id
-    assert len(all_docs) == 32123
+    assert "8811478" == next(iter(dataset)).doc_id
+    assert len(dataset) == 32123
 
 
 def test_lsr_benchmark_load_queries():
