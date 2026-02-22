8de6f73e7b90dfd837bba11fd96bcde47fc1ae5d
Rayk Kretzschmar <112646288+RaykKretzschmar@users.noreply.github.com>
2026-02-22 17:34:35 +0100

    Refactor docstring in callbacks module to explain callbacks

--- lightning_ir/callbacks/__init__.py
+++ lightning_ir/callbacks/__init__.py
@@ -2,9 +2,18 @@
 Module containing Lightning IR callbacks.
 
 This module provides PyTorch Lightning compatible callbacks for various stages and tasks.
+
+Callbacks let you to customize the training and evaluation process in PyTorch Lightning, allowing you to
+execute code at specific points during the training loop, such as after each epoch or batch.
 """
 
-from .callbacks import IndexCallback, RankCallback, RegisterLocalDatasetCallback, ReRankCallback, SearchCallback
+from .callbacks import (
+    IndexCallback,
+    RankCallback,
+    RegisterLocalDatasetCallback,
+    ReRankCallback,
+    SearchCallback,
+)
 
 __all__ = [
     "IndexCallback",
